9d495fbb5d9048708f3076b0132bad00e785b306
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-03-09 17:06:12 -0700

    created login page, added CSS styling

--- express_server.js
+++ express_server.js
@@ -47,6 +47,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies.username
+  };
+  res.render("login", templateVars);
+});
+
 app.post("/urls", (req, res) => {
   let short = generateRandomString();
   urlDatabase[short] = req.body.longURL;
